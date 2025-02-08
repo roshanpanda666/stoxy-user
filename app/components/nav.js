eb86693bf0f8f2d7fdecdfe29411d4e86571a384
roshan <roshanpanda666@gmail.com>
2025-02-08 21:36:45 +0530

    commiting

---
+++ app/components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <div className='flex justify-end item-center gap-10 mr-4'>
+                <div>
+                    my-orders
+                </div>
+                <div>
+                    me
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ProductList from './components/productlist'
+import Nav from './components/nav'
 
 const page = () => {
   return (
     <div>
-      hello world
+      user-stoxy
+      <div>
+        <Nav></Nav>
+      </div>
       <div>
         <ProductList></ProductList>
       </div>
