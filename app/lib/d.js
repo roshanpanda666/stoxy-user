0bc989ed17ca705ed77d0fc25e3532b5c8c41ea1
roshan <roshanpanda666@gmail.com>
2025-05-29 22:08:56 +0530

    ...

--- app/lib/d.js
+++ app/lib/d.js
@@ -2,5 +2,3 @@ export const{username,password}=process.env
 
 export const connectionSRT="mongodb+srv://"+username+":"+password+"@cluster0.tskix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-console.log(username);
-console.log(password);
